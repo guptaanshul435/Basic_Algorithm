3f62b1760e8b7d7a1e855c639a5feec4397e10be
Anshul Gupta <guptaanshul435@gmail.com>
2024-02-16 08:31:37 +0530

    BinarySearchTree_add_search&delete

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -21,6 +21,31 @@ class BinarySearchTree{
  }
  
  
+ //delete
+ public boolean delete(BinaryTreeNode root, int ele){
+	 if(root==null){
+		 return false;
+	 }
+	 BinaryTreeNode node=searchNode(root,ele);
+	 deleteEle(node);
+	 return true;
+	 
+ }
+ 
+ private void deleteEle(BinaryTreeNode found){
+	 
+	 if(found.right!=null){
+		 found.ele=found.right.ele;
+		 deleteEle(found.right);
+	 }else{
+		 found.ele=-1;
+		 found=null;
+		 
+	 }
+	 
+ }
+ 
+ 
  // preOrder
  public void preOrder(BinaryTreeNode node){
 	 if(node!=null){ 
@@ -48,10 +73,39 @@ class BinarySearchTree{
 	 }
  }
  
+ //Binary Search note
+ private BinaryTreeNode searchNode(BinaryTreeNode root,int ele){
+	 
+	 if((int)root.ele==ele){
+		 return root;
+	 }else if((int)root.ele>=ele && root.left!=null){
+		return searchNode(root.left,ele);
+	 }else if((int)root.ele<ele && root.right!=null){
+		 return searchNode(root.right,ele);
+	 }else{
+		 return null;
+	 }
+		 
+	 
+ }
  
- //Binary Search 
- public int search(int ele){
-	 return 1;
+ 
+ 
+ 
+ //Binary Search ele 
+ public int search(BinaryTreeNode root,int ele){
+	 
+	 if((int)root.ele==ele){
+		 return ele;
+	 }else if((int)root.ele>=ele && root.left!=null){
+		return search(root.left,ele);
+	 }else if((int)root.ele<ele && root.right!=null){
+		 return search(root.right,ele);
+	 }else{
+		 return -1;
+	 }
+		 
+	 
  }
 
 
@@ -66,16 +120,24 @@ public static void main(String[] args){
 	binaryTree[4]=new BinaryTreeNode(6);
 	binaryTree[5]=new BinaryTreeNode(4);
 	binaryTree[6]=new BinaryTreeNode(2);
-	binaryTree[7]=new BinaryTreeNode(-3);
+	binaryTree[7]=new BinaryTreeNode(10);
+	
 	
 	
 	BinarySearchTree bst=new BinarySearchTree();
 	for(BinaryTreeNode b:binaryTree){
 	bst.insert(node,b);
 	}
+	
+	//search
+	int found=bst.search(node,5);
+	System.out.println("if found ele is -1 means your ele is not found in the tree");
+	System.out.printf("The elemet got found:%d\n",found);
+	
 	//System.out.println(node);
 	bst.preOrder(node);
 	
+	System.out.println(bst.delete(node,5));
 	
 	System.out.println("*************************************");
 	bst.inOrder(node);
